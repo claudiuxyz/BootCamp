311a3c8fe763827be69dc0be8f1efb76fb081223
Claudiu Balanean <Claudiu.Balanean@lasting.ro>
2018-02-03 23:02:50 +0200

    Add Factory Method project

---
+++ DesignPatterns/C#/FactoryMethod/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FactoryMethod
+{
+    //---------- Interfaces & abstract classes ---------------
+    public interface IDocumentTypes
+    {
+    }
+    public abstract class DocumentGenerator
+    {
+        public abstract IDocumentTypes FactoryMethod();
+
+        public void NewDocument()
+        {
+
+        }
+
+        public void PrintDocument()
+        {
+        }
+    }
+
+    //---------- Concrete documents ---------------
+    public class HtmlDoc : IDocumentTypes
+    {
+
+    }
+    public class XmlDoc : IDocumentTypes
+    {
+
+    }
+    public class TextDoc : IDocumentTypes
+    {
+
+    }
+    //---------- Concrete document generators ---------------
+    public class HtmlGenerator: DocumentGenerator
+    {
+        public override IDocumentTypes FactoryMethod()
+        {
+            return new HtmlDoc();
+        }
+    }
+
+    public class XmlGenerator : DocumentGenerator
+    {
+        public override IDocumentTypes FactoryMethod()
+        {
+            return new XmlDoc();
+        }
+    }
+
+    public class TextGenerator : DocumentGenerator
+    {
+        public override IDocumentTypes FactoryMethod()
+        {
+            return new TextDoc();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
