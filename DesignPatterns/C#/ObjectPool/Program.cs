1fcc905a3a53c0d391792a6975aa9327f75a5657
Claudiu Balanean <claudiuxyz@yahoo.com>
2018-03-16 18:34:31 +0200

    Object Pool - partial

--- DesignPatterns/C#/ObjectPool/Program.cs
+++ DesignPatterns/C#/ObjectPool/Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace ObjectPool
 {
     class Program
     {
+        public class PrinterDevice
+        {
+            private static PrinterDevice m_instance;
+            public static PrinterDevice Instance()
+            {
+                if (m_instance == null)
+                {
+                    m_instance = new PrinterDevice();
+                }
+                return m_instance;
+            }
+
+            public void Print(string text)
+            {
+                Console.WriteLine(text);
+            }
+        }
+
+        public class ConnectionToPrinter
+        {
+            public void SendData(string text)
+            {
+                PrinterDevice.Instance().Print(text);
+            }
+        }
+
+        public class ObjectPool<T> where T : new()
+        {
+            private readonly int MAX_ITEMS_COUNT = 5;
+
+            private readonly ConcurrentBag<T> items = new ConcurrentBag<T>();
+
+            private int itemsCount = 0;
+
+            public void ReleaseResouce(T item)
+            {
+                if(itemsCount<MAX_ITEMS_COUNT)
+                {
+                    items.Add(item);
+                    itemsCount++;
+                }
+            }
+
+            public T Get()
+            {
+                T item;
+                if(items.TryTake(out item))
+                {
+                    itemsCount--;
+                    return item;
+                }
+                else
+                {
+                    T obj = new T();
+                    items.Add(obj);
+                    itemsCount++;
+                    return obj;
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            ObjectPool<ConnectionToPrinter> objectPool = new ObjectPool<ConnectionToPrinter>();
+            var c = objectPool.Get();
+            c.SendData("Test");
+            objectPool.ReleaseResouce(c);
+
+            Console.ReadKey();
         }
     }
 }
