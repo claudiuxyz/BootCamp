cd0647f0948162a62fb84eef9fe1ee02744fa316
Alexandra Herte <hertealexandra@gmail.com>
2018-04-01 09:14:05 +0300

    Bridge-java

---
+++ DesignPatterns/Java/Bridge/src/com/ale/Circle.java
@@ -0,0 +1,14 @@
+package com.ale;
+
+public class Circle extends Shape {
+
+    public Circle(Color color) {
+        super(color);
+    }
+
+    @Override
+    public void applyColor() {
+        System.out.print("Triangle filled with color ");
+        color.applyColor();
+    }
+}
