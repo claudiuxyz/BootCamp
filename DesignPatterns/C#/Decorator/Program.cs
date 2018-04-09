3e4ef2d7e0ee74d142bd2ad83d460e665ad3c201
Claudiu Balanean <Claudiu.Balanean@lasting.ro>
2018-04-09 23:47:18 +0300

    Decorator - C#

---
+++ DesignPatterns/C#/Decorator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Decorator
+{
+    //-------- Component --------------
+    public interface EmailBody //Component
+    {
+        void LoadBody();
+    }
+
+    public class Email : EmailBody // Concrete Component
+    {
+        public void LoadBody()
+        {
+            Console.WriteLine("Main email body");
+        }
+    }
+
+    //-------- Decorators --------------
+
+    public abstract class EmailDecorator : EmailBody // Base Decorator
+    {
+        public EmailDecorator(EmailBody emailBody)
+        {
+            m_emailBody = emailBody;
+        }
+        public abstract void LoadBody();
+
+        protected EmailBody m_emailBody;
+    }
+
+    public class ChristmasDecorator : EmailDecorator
+    {
+        public ChristmasDecorator(EmailBody emailBody): base(emailBody) { }
+
+        public override void LoadBody()
+        {
+            Console.WriteLine("Christmas content");
+            m_emailBody.LoadBody();
+        }
+    }
+
+    public class NewYearDecorator : EmailDecorator
+    {
+        public NewYearDecorator(EmailBody emailBody) : base(emailBody) { }
+
+        public override void LoadBody()
+        {
+            Console.WriteLine("New Year content");
+            m_emailBody.LoadBody();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var email = new Email();
+            var xmasEmail = new ChristmasDecorator(email);
+            var bothHolidaysEmail = new NewYearDecorator(xmasEmail);
+
+            bothHolidaysEmail.LoadBody();
+            Console.ReadKey();
+        }
+    }
+}
