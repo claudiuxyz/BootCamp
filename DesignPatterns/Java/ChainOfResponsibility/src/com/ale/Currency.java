8a12eeec49fe4e551762da0135d28c04a0041ea1
Alexandra Herte <hertealexandra@gmail.com>
2018-04-09 09:20:25 +0300

    ChainOfResponsibility-java

---
+++ DesignPatterns/Java/ChainOfResponsibility/src/com/ale/Currency.java
@@ -0,0 +1,16 @@
+package com.ale;
+
+public class Currency
+{
+    private int amount;
+
+    public Currency(int amount)
+    {
+        this.amount = amount;
+    }
+
+    public int getAmount()
+    {
+        return this.amount;
+    }
+}
