d51f556deb78cd08273efd10d0e0cce308573a61
Alexandra Herte <hertealexandra@gmail.com>
2018-04-09 09:22:00 +0300

    Command-java

---
+++ DesignPatterns/Java/Command/src/com/ale/CommandPatternDemo.java
@@ -0,0 +1,21 @@
+package com.ale;
+
+public class CommandPatternDemo {
+
+    public static void main(String[] args)
+    {
+        FileSystemReceiver fs = FileSystemReceiverUtil.getUnderlyingFileSystem();
+
+        OpenFileCommand openFileCommand = new OpenFileCommand(fs);
+        FileInvoker file = new FileInvoker(openFileCommand);
+        file.execute();
+
+        WriteFileCommand writeFileCommand = new WriteFileCommand(fs);
+        file = new FileInvoker(writeFileCommand);
+        file.execute();
+
+        CloseFileCommand closeFileCommand = new CloseFileCommand(fs);
+        file = new FileInvoker(closeFileCommand);
+        file.execute();
+    }
+}
